255acfa1e346464743e7dd74e90ff4ef3735ff81
lota <tjrals120@gmail.com>
2025-06-06 13:58:40 +0900

    upload stable legacy code

---
+++ sub3_legacy.s
@@ -0,0 +1,41 @@
+        AREA    RESET, CODE, READONLY
+        ENTRY
+        EXPORT  _main
+
+_main
+        LDR     R0, =0x40000036      
+        LDR     R1, =0x40100000      
+        MOV     R2, #9600           
+
+        CMP     R2, #0
+        BEQ     End
+
+LoopPixel
+        
+        LDRB    R3, [R0]            
+        MOV     R4, R3
+        ADD     R3, R3, R4           
+        ADD     R3, R3, R4           
+
+       
+        LDRB    R4, [R0, #1]         ; G
+        MOV     R5, R4
+        ADD     R4, R4, R5           ; 2*G
+        ADD     R4, R4, R5           ; 3*G
+        ADD     R4, R4, R5           ; 4*G
+        ADD     R4, R4, R5           ; 5*G
+        ADD     R4, R4, R5           ; 6*G
+        ADD     R3, R3, R4
+
+        LDRB    R4, [R0, #2]         
+        ADD     R3, R3, R4           
+        STRH    R3, [R1]
+        ADD     R0, R0, #4
+        ADD     R1, R1, #2
+        SUBS    R2, R2, #1
+        BNE     LoopPixel
+
+End
+        B       End
+
+        END
